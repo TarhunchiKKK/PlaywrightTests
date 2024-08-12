ae33b9f4f5b62e21a974e8b93a099e825cc1137e
konstantin <konstabarilo12@gmail.com>
2024-08-12 16:43:19 +0300

    test: /auth/users/me/

--- TeachMeSkills/tests/auth/users-me.spec.ts
+++ TeachMeSkills/tests/auth/users-me.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
-import { getTokens } from '../../utils';
+import { CredentailsGenerator, getTokens } from '../../utils';
 import { API_URL, STATUS_CODES } from '../../constants';
-import { UserSchema } from '../../types';
+import { PatchedUserRequest, UserRequest, UserSchema } from '../../types';
 
 let accessToken: string;
 const authHeaders = {
@@ -15,7 +15,7 @@ test.describe('/auth/users/me/', () => {
     });
 
     test('GET', async ({ request }) => {
-        const response = await request.get(`${API_URL}/auth/users`, {
+        const response = await request.get(`${API_URL}/auth/users/me/`, {
             headers: {
                 ...authHeaders,
             },
@@ -24,5 +24,48 @@ test.describe('/auth/users/me/', () => {
 
         const user = UserSchema.parse(await response.json());
         expect(user.id).toBeDefined();
+    });
+
+    test('PUT', async ({ request }) => {
+        const generatedUsername = CredentailsGenerator.generateUsername();
+
+        const requestBody: UserRequest = {
+            username: generatedUsername,
+        };
+
+        const response = await request.put(`${API_URL}/auth/users/me/`, {
+            headers: {
+                ...authHeaders,
+            },
+            data: requestBody,
+        });
+        expect(response.status()).toEqual(STATUS_CODES.OK);
+
+        const user = UserSchema.parse(await response.json());
+        expect(user.username).toEqual(generatedUsername);
+    });
+
+    test('PATCH', async ({ request }) => {
+        const generatedUsername = CredentailsGenerator.generateUsername();
+
+        const requestBody: PatchedUserRequest = {
+            username: generatedUsername,
+        };
+
+        const response = await request.put(`${API_URL}/auth/users/me/`, {
+            headers: {
+                ...authHeaders,
+            },
+            data: requestBody,
+        });
+        expect(response.status()).toEqual(STATUS_CODES.OK);
+
+        const user = UserSchema.parse(await response.json());
+        expect(user.username).toEqual(generatedUsername);
+    });
+
+    test.skip('DELETE', async ({ request }) => {
+        const response = await request.delete(`${API_URL}/auth/users/me/`);
+        expect(response.status()).toEqual(STATUS_CODES.NO_CONTENT);
     });
 });
